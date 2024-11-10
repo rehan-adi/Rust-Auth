6dca893838e0371cda78d716af2b85ba91f61dcf
Rehan <rehanalire52@gmail.com>
2024-11-10 20:35:32 +0530

    feat: Set up Actix Web server and create health check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+
+mod api;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    dotenv().ok();
+
+    let port = std::env::var("PORT").expect("ENV not found");
+    println!("Server is running on port {}", port);
+
+    HttpServer::new(|| {
+           App::new().service(api::healthcheck::health_check)
+    })
+    .bind(format!("localhost:{}", port))?
+    .run()
+    .await
 }
