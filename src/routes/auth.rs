16b846a3663fcf517907b8680592b4891cf15bfc
Rehan <rehanalire52@gmail.com>
2024-11-20 23:54:22 +0530

    feat: add Signin route

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,8 +1,12 @@
-use diesel::RunQueryDsl;
-use bcrypt::{hash, DEFAULT_COST};
-use crate::types::auth::SignupData;
+use chrono::{Utc};
+use diesel::prelude::*;
+use diesel::associations::HasTable;
+use bcrypt::{hash, verify, DEFAULT_COST};
+use jsonwebtoken::{encode, Header, EncodingKey};
+use crate::schema::users::dsl::{users, email};
 use actix_web::{post, web, HttpResponse, Responder};
-use crate::{config::db::DbPool, models::user::NewUser, schema::users};
+use crate::types::auth::{SigninData, SignupData, Claims};
+use crate::{config::db::DbPool, models::user::{User, NewUser}};
 
 #[post("/signup")]
 pub async fn signup(
@@ -21,12 +25,47 @@ pub async fn signup(
         is_login: Some(false),
     };
 
-    match diesel::insert_into(users::table)
+    match diesel::insert_into(users::table())
     .values(&new_user)
     .execute(conn)
 {
     Ok(_) => HttpResponse::Created().body("User created successfully"),
     Err(_) => HttpResponse::InternalServerError().body("Error creating user"),
 }
+
+}
+
+
+#[post("/signin")]
+pub async fn signin(
+    pool: web::Data<DbPool>,
+    data: web::Json<SigninData>
+) -> impl Responder {
+
+    let conn = &mut pool.get().expect("Failed to get DB connection");
+    
+    let result = users
+    .filter(email.eq(&data.email))
+    .first::<User>(conn);
+
+    match result {
+        Ok(user) => {
+            if verify(&data.password, &user.password).unwrap_or(false) {
+                
+                let expiration = 3600;
+                // let claims = Claims {
+                //     sub: user.email.clone(),
+                //     exp: (Utc::now().timestamp() + expiration) as usize,
+                // };
+
+                HttpResponse::Ok().body("Signin successful")
+            } else {
+                HttpResponse::Unauthorized().body("Invalid email or password")
+            }
+        }
+        Err(_) => {
+            HttpResponse::NotFound().body("User not found")
+        }
+    }
 
 }
--- src/types/auth.rs
+++ src/types/auth.rs
@@ -1,8 +1,20 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 pub struct SignupData {
     pub username: String,
     pub email: String,
     pub password: String,
+}
+
+#[derive(Deserialize)]
+pub struct SigninData {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    sub: String,
+    exp: usize, 
 }
