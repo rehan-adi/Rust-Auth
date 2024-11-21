cf673cd09dabe14d8260e2fdd2203daac308c7a2
Rehan <rehanalire52@gmail.com>
2024-11-21 21:28:44 +0530

    feat: Add validation to SigninData

--- src/types/auth.rs
+++ src/types/auth.rs
@@ -13,9 +13,12 @@ pub struct SignupData {
     pub password: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Validate, Deserialize)]
 pub struct SigninData {
+    #[validate(email(message = "Invalid email format"))]
     pub email: String,
+
+    #[validate(length(min = 6, message = "Password must be at least 6 characters long"))]
     pub password: String,
 }
 
